286c8dc4f782a9b24b4f687a72e0a601b858dd1d
Nathan-422 <natiouswright@gmail.com>
2022-11-17 19:43:48 -0600

    Added flight status text update and background color update

--- scripts.js
+++ scripts.js
@@ -3,8 +3,12 @@
 window.addEventListener("load", function() {
     console.log("Window loaded");
 
-    const takeOff = document.getElementById("takeoff")
+    const takeOff = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
     takeOff.addEventListener("click", function() {
         window.confirm("Confirm that the shuttle is ready for takeoff.");
+        flightStatus.innerHTML = "Shuttle in flight."
+        shuttleBackground.style.backgroundColor = "blue";
     });
 });
